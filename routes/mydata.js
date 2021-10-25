0aa23cad07c0907daf23b2a84f549d7d71f02938
Chaitanya Swaroop <62442765+chaitanyaswaroopdev@users.noreply.github.com>
2021-10-25 13:17:15 -0500

    added stuff

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("mydata", { title: "Chaithanya swaroop", fact: "my fact" });
+  res.render("mydata", {
+    title: "Chaitanya Swaroop Udata",
+    fact: "I love to cook maggie from scratch",
+  });
 });
 
 module.exports = router;
