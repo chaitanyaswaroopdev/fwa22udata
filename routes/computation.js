b7530f5da0b2ae2ad1c0105d1900a43b4ead30f1
Chaitanya Swaroop <62442765+chaitanyaswaroopdev@users.noreply.github.com>
2021-10-25 22:40:08 -0500

    Bonus

--- routes/computation.js
+++ routes/computation.js
@@ -8,19 +8,21 @@ var cbrt;
 /* GET users listing. */
 
 router.get("/", function (req, res, next) {
-  rno = Math.round(Math.random() * (100 - 1) + 1);
+  rno = Math.random() * (100 - 1) + 1;
+  rno1 = Math.random() * (100 - 1) + 1;
 
-  atan2 = Math.atan2(rno);
+  atan2 = Math.atan2(rno, rno1);
 
-  atanh = Math.atanh(rno);
+  atanh = Math.atanh(0.5);
 
   cbrt = Math.cbrt(rno);
 
-  res.send(`atan2 applied to ${rno} is ${atan2} <br>
+  res.send(`atan2 applied to ${rno}, ${rno1} is ${atan2} <br>
 
-            atanh applied to ${rno} is ${atanh} <br>
+            atanh applied to 0.5 is ${atanh} <br>
 
             cbrt applied to ${rno} is ${cbrt}`);
 });
 
 module.exports = router;
+
